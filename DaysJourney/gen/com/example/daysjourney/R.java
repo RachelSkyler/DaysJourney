f7342ad39da8b3ff69b9b49cd625ecb713a2c232
potatolylc <superpotato@163.com>
2014-04-01 19:44:54 +0900

    Layout for Android V.1

--- DaysJourney/gen/com/example/daysjourney/R.java
+++ DaysJourney/gen/com/example/daysjourney/R.java
@@ -956,6 +956,7 @@ containing a value of this type.
          */
         public static final int activity_horizontal_margin=0x7f080015;
         public static final int activity_vertical_margin=0x7f080016;
+        public static final int button_width=0x7f080019;
         /**  The platform's desired fixed height for a dialog along the major axis
          (the screen is in portrait). This may be either a fraction or a dimension.
  The platform's desired fixed height for a dialog along the major axis
@@ -988,6 +989,10 @@ containing a value of this type.
          (the screen is in portrait). This may be either a fraction or a dimension.
          */
         public static final int dialog_fixed_width_minor=0x7f080012;
+        public static final int margin_side=0x7f080018;
+        /**  Numbers 
+         */
+        public static final int margin_top=0x7f080017;
     }
     public static final class drawable {
         public static final int abc_ab_bottom_solid_dark_holo=0x7f020000;
@@ -1077,7 +1082,11 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int boxblue_leftround=0x7f020057;
+        public static final int boxblue_rightround=0x7f020058;
+        public static final int boxred_leftround=0x7f020059;
+        public static final int boxred_rightround=0x7f02005a;
+        public static final int ic_launcher=0x7f02005b;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1088,10 +1097,11 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
+        public static final int action_forgot_password=0x7f05004b;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f05004a;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1102,6 +1112,7 @@ containing a value of this type.
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
+        public static final int email=0x7f050041;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
@@ -1116,7 +1127,8 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int pager=0x7f05003d;
+        public static final int password=0x7f050042;
+        public static final int password_confirm=0x7f050048;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1130,11 +1142,21 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
-        public static final int section_label=0x7f05003c;
+        public static final int section_label=0x7f050049;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int sign_in=0x7f050043;
+        public static final int sign_in_button=0x7f05003d;
+        public static final int sign_in_form=0x7f050040;
+        public static final int sign_in_status=0x7f05003e;
+        public static final int sign_in_status_message=0x7f05003f;
+        public static final int sign_up=0x7f050047;
+        public static final int sign_up_button=0x7f05003c;
+        public static final int sign_up_form=0x7f050046;
+        public static final int sign_up_status=0x7f050044;
+        public static final int sign_up_tatus_message=0x7f050045;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
@@ -1194,12 +1216,17 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int fragment_main=0x7f030018;
-        public static final int main=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_main=0x7f030018;
+        public static final int activity_sign_in=0x7f030019;
+        public static final int activity_sign_up=0x7f03001a;
+        public static final int activity_splash_screen=0x7f03001b;
+        public static final int fragment_main=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
+        public static final int sign_in=0x7f0c0001;
+        public static final int sign_up=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1242,16 +1269,49 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a0011;
+        public static final int action_forgot_password=0x7f0a001e;
+        public static final int action_settings=0x7f0a000e;
+        public static final int action_sign_in=0x7f0a001d;
+        public static final int action_sign_up=0x7f0a0025;
+        /**  Text 
+         */
         public static final int app_name=0x7f0a000d;
-        public static final int title_section1=0x7f0a000e;
-        public static final int title_section2=0x7f0a000f;
-        public static final int title_section3=0x7f0a0010;
+        public static final int dummy_button=0x7f0a0018;
+        public static final int dummy_content=0x7f0a0019;
+        public static final int error_field_required=0x7f0a0023;
+        public static final int error_incorrect_password=0x7f0a0022;
+        public static final int error_invalid_email=0x7f0a0020;
+        public static final int error_invalid_password=0x7f0a0021;
+        public static final int main_home=0x7f0a000f;
+        /**  Strings related to login 
+ Strings related to login 
+         */
+        public static final int prompt_email=0x7f0a001b;
+        public static final int prompt_password=0x7f0a001c;
+        public static final int prompt_password_confirm=0x7f0a0024;
+        public static final int sign_in_progress=0x7f0a001f;
+        public static final int sign_up_progress=0x7f0a0026;
+        public static final int title_activity_fullscreen=0x7f0a0017;
+        public static final int title_activity_intro_splash=0x7f0a001a;
+        /**  Activity titles 
+         */
+        public static final int title_activity_main=0x7f0a0010;
+        public static final int title_activity_sign_in=0x7f0a0012;
+        public static final int title_activity_sign_up=0x7f0a0013;
+        public static final int title_activity_splash_screen=0x7f0a0011;
+        /**  Titles for swipe pages 
+         */
+        public static final int title_section1=0x7f0a0014;
+        public static final int title_section2=0x7f0a0015;
+        public static final int title_section3=0x7f0a0016;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
+
+
     
 
             Theme customizations available in newer API levels can go in
@@ -1261,6 +1321,8 @@ containing a value of this type.
 
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
+
+
     
  API 11 theme customizations can go here. 
 
@@ -1275,6 +1337,9 @@ containing a value of this type.
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f0b008c;
+        public static final int LoginFormContainer=0x7f0b008f;
+        public static final int SigninFormContainer=0x7f0b008d;
+        public static final int SignupFormContainer=0x7f0b008e;
         /**  Mimic text appearance in select_dialog_item.xml 
          */
         public static final int TextAppearance_AppCompat_Base_CompactMenu_Dialog=0x7f0b0063;
--- DaysJourney/src/com/example/daysjourney/MainActivity.java
+++ DaysJourney/src/com/example/daysjourney/MainActivity.java
@@ -1,53 +1,55 @@
 package com.example.daysjourney;
 
-import java.util.Locale;
-
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a {@link FragmentPagerAdapter}
-	 * derivative, which will keep every loaded fragment in memory. If this
-	 * becomes too memory intensive, it may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
+	public class ButtonClickHandler implements OnClickListener {
 
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
-	ViewPager mViewPager;
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			int viewId = v.getId();
+			switch (viewId) {
+			case R.id.sign_in_button:
+				Intent intent1 = new Intent(MainActivity.this, SignInActivity.class);
+				startActivity(intent1);
+				break;
+
+			case R.id.sign_up_button:
+				Intent intent2 = new Intent(MainActivity.this, SignUpActivity.class);
+				startActivity(intent2);
+				break;
+
+			default:
+				break;
+			}
+		}
+
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.activity_main);
 
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the activity.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
+		Button signUpBtn = (Button) this.findViewById(R.id.sign_up_button);
+		Button signInBtn = (Button) this.findViewById(R.id.sign_in_button);
 
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-
+		signUpBtn.setOnClickListener(new ButtonClickHandler());
+		signInBtn.setOnClickListener(new ButtonClickHandler());
 	}
 
 	@Override
@@ -68,82 +70,6 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a PlaceholderFragment (defined as a static inner class
-			// below).
-			return PlaceholderFragment.newInstance(position + 1);
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 3;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_section3).toUpperCase(l);
-			}
-			return null;
-		}
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		private static final String ARG_SECTION_NUMBER = "section_number";
-
-		/**
-		 * Returns a new instance of this fragment for the given section number.
-		 */
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			TextView textView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			textView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
 	}
 
 }
